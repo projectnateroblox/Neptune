40aa5a21629bef08445e1877d5bfb1c8cd16ec22
direkzz <dirgahamdani71@gmail.com>
2025-03-29 22:58:10 +0700

    Remove ignored files from Git tracking

--- scripts/lunor_fisch.lua
+++
@@ -1,4 +0,0 @@
-
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Just3itx/Lunor-Loadstrings/refs/heads/main/Loader"))()
-
--- scripts/nativehub.lua
+++
@@ -1,2 +0,0 @@
-script_key="";
-(loadstring or load)(game:HttpGet("https://raw.githubusercontent.com/Native-lab/Native/main/loader.lua"))()
